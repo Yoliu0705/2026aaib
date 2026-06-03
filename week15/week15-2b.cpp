6d7afe7cbf0cf9614c83448dead008580376972c
Yoliu0705 <linxliu2007@gmail.com>
2026-06-03 11:38:31 +0800

    week15

---
+++ week15/week15-2b.cpp
@@ -0,0 +1,13 @@
+// week15-2b.cpp SOIT106 ADVANCE_011
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int a = N / 1000;
+    int b = N / 100 % 10;
+    int c = N / 10 % 10;
+    int d = N % 10;
+    printf("%d\n", a*8 + b*4 + c*2 + d);
+}
